c8f8c8e35f065b0cadc7c00b1ad4855991a32380
miroslaw.janas <miroslaw.janas@N-MUC-2368>
2018-04-24 15:35:30 +0200

    test

--- fsd.exe/Properties/AssemblyInfo.cs
+++ fsd.exe/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("fsd.exe")]
+[assembly: AssemblyTitle("MobileDeviceUpdate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("fsd.exe")]
+[assembly: AssemblyProduct("MobileDeviceUpdate")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
